ffa41b6c1cea1dd34f0147d96ba4ad63b03d77de
datadiskpfv <paul.valle@datadisk.co.uk>
2019-01-13 15:33:18 +0000

    added delete method to todo controller

--- src/main/java/uk/co/datadisk/webservices/restfulwebservices/controllers/TodoController.java
+++ src/main/java/uk/co/datadisk/webservices/restfulwebservices/controllers/TodoController.java
@@ -1,14 +1,14 @@
 package uk.co.datadisk.webservices.restfulwebservices.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import uk.co.datadisk.webservices.restfulwebservices.entities.Todo;
 import uk.co.datadisk.webservices.restfulwebservices.services.TodoHardcodedService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TodoController {
 
@@ -18,5 +18,16 @@ public class TodoController {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoService.findAll();
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+
+        Todo todo = todoService.deleteById(id);
+        if(todo != null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/uk/co/datadisk/webservices/restfulwebservices/services/TodoHardcodedService.java
+++ src/main/java/uk/co/datadisk/webservices/restfulwebservices/services/TodoHardcodedService.java
@@ -14,12 +14,34 @@ public class TodoHardcodedService {
     private static int idCounter = 0;
 
     static {
-        todos.add(new Todo(++idCounter, "Paul Valle", "Learn to program in Java", new Date(), false));
+        todos.add(new Todo(++idCounter, "Paul Valle", "Learn to program in Java 11", new Date(), false));
         todos.add(new Todo(++idCounter, "Paul Valle", "Learn to program in Angular 7", new Date(), false));
         todos.add(new Todo(++idCounter, "Paul Valle", "Learn to program in JavaScript", new Date(), false));
     }
 
     public List<Todo> findAll() {
         return todos;
+    }
+
+    public Todo deleteById(long id) {
+        Todo todo = findById(id);
+        if(todo == null) {
+            return null;
+        }
+
+        if(todos.remove(todo)) {
+            return todo;
+        }
+
+        return null;
+    }
+
+    public Todo findById(long id) {
+        for(Todo todo: todos) {
+            if(todo.getId() == id){
+                return todo;
+            }
+        }
+        return null;
     }
 }
