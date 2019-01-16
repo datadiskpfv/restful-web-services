c10a6ffb339028f326d56975a2286676807459a9
pvalle <paul.valle@orbian.com>
2019-01-16 08:52:18 +0000

    working json web token (JWT) implementation

--- src/main/java/uk/co/datadisk/webservices/restfulwebservices/controllers/BasicAuthenticationController.java
+++ src/main/java/uk/co/datadisk/webservices/restfulwebservices/controllers/BasicAuthenticationController.java
@@ -1,18 +1,18 @@
-package uk.co.datadisk.webservices.restfulwebservices.controllers;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import uk.co.datadisk.webservices.restfulwebservices.beans.AuthenticationBean;
-
-@CrossOrigin(origins="http://localhost:4200")
-@RestController
-public class BasicAuthenticationController {
-
-    @GetMapping(path="/basicauth")
-    public AuthenticationBean helloWorldBean() {
-        // throw new RuntimeException("Some error has happened, please contact support");
-        System.out.println("You tried to login");
-        return new AuthenticationBean("You are authenticated");
-    }
-}
+//package uk.co.datadisk.webservices.restfulwebservices.controllers;
+//
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import uk.co.datadisk.webservices.restfulwebservices.beans.AuthenticationBean;
+//
+//@CrossOrigin(origins="http://localhost:4200")
+//@RestController
+//public class BasicAuthenticationController {
+//
+//    @GetMapping(path="/basicauth")
+//    public AuthenticationBean helloWorldBean() {
+//        // throw new RuntimeException("Some error has happened, please contact support");
+//        System.out.println("You tried to login");
+//        return new AuthenticationBean("You are authenticated");
+//    }
+//}
